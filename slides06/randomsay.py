a6a523bac5551cb6a759bb9b30a56ff8e793135b
Peter Bui <pbui@nd.edu>
2026-08-30 14:53:54 -0400

    Slides 06: add randomsay

---
+++ slides06/randomsay.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import random                           # Discuss: random module
+import sys
+
+# Constants
+                                        # Discuss: set data structure
+FORBIDDEN = {'bong', 'sodomized', 'kiss', 'head-in', 'satanic', 'telebears'}
+
+# Main Execution
+
+def main():
+    cows = []                           # Discuss: os.popen
+    for index, line in enumerate(os.popen('cowsay -l')):
+        if not index:                   # Discuss: enumerate
+            continue
+
+        for cow in line.split():        # Review: str.split
+            if cow not in FORBIDDEN:    # Review: searching collection
+                cows.append(cow)        # Review: list.append
+    
+    command = ['cowsay', '-f', random.choice(cows)] + sys.argv[1:]
+    subprocess.run(command, check=True) # Variant: os.system
+
+if __name__ == '__main__':
+    main()
