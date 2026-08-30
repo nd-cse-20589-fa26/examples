39cfbc9547eb03e7f2dabf5407395fd73816e303
Peter Bui <pbui@nd.edu>
2026-08-30 15:37:28 -0400

    Slides 06: use realpath and display error if not found

--- slides06/which.py
+++ slides06/which.py
@@ -9,7 +9,8 @@ bin_paths = os.environ['PATH'].split(':')
 for bin_path in bin_paths:
     target_path = os.path.join(bin_path, target)
     if os.access(target_path, os.X_OK):
-        print(target_path)
+        print(os.path.realpath(target_path))
         sys.exit(0)
 
+print(f'no {target} in ({os.environ["PATH"]})')
 sys.exit(1)
