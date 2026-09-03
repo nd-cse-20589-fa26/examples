a3eea7867e62434b1561df7e789e4fd9563f689e
Peter Bui <pbui@nd.edu>
2026-09-03 14:56:46 -0400

    Slides 06: standardize arguments

--- slides06/randomsay.py
+++ slides06/randomsay.py
@@ -11,7 +11,7 @@ FORBIDDEN = {'bong', 'sodomized', 'kiss', 'head-in', 'satanic', 'telebears'}
 
 # Main Execution
 
-def main():
+def main(arguments: list[str]=sys.argv[1:]) -> None:
     cows = []                           # Discuss: os.popen
     for index, line in enumerate(os.popen('cowsay -l')):
         if not index:                   # Discuss: enumerate
@@ -21,7 +21,7 @@ def main():
             if cow not in FORBIDDEN:    # Review: searching collection
                 cows.append(cow)        # Review: list.append
     
-    command = ['cowsay', '-f', random.choice(cows)] + sys.argv[1:]
+    command = ['cowsay', '-f', random.choice(cows)] + arguments
     subprocess.run(command, check=True) # Variant: os.system
 
 if __name__ == '__main__':
